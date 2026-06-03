779b7b224debf1c987c2b9ab14fa96494cba4a7c
vanessalimadev <vanessaaparecida5289@gmail.com>
2026-06-03 20:37:11 -0300

    feat: adiciona icone de prioridade na exibicao de tarefas

--- taskmanager/utils.py
+++ taskmanager/utils.py
@@ -2,7 +2,13 @@
 
 def format_task(task):
     status = "[ ]"
-    return f"{status} [{task['priority']}] #{task['id']} - {task['title']}"
+    priority_icons = {
+        "high": "🔥",
+        "normal": "🔷",
+        "low": "🟢",
+    }
+    icon = priority_icons.get(task.get("priority", "normal"), "🔷")
+    return f"{status} {icon} [{task['priority']}] #{task['id']} - {task['title']}"
 
 def filter_tasks(tasks, show_done=True):
     if show_done:
