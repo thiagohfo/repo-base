e4fe308ff0fbfe2b960ca7e571c3d67fa9dc17de
Igor Vinicius <igorviniciusalves77@gmail.com>
2026-06-04 16:26:38 -0300

    feat: add filter by priority

--- taskmanager/utils.py
+++ taskmanager/utils.py
@@ -1,10 +1,11 @@
 # Zona de conflito intencional: ambos os devs modificarão format_task e filter_tasks
 
-def format_task(task):
-    status = "[ ]"
-    return f"{status} [{task['priority']}] #{task['id']} - {task['title']}"
+
+def filter_priority(tasks, priority):
+    return [task for task in tasks if task.get("priority") == priority]
 
 def filter_tasks(tasks, show_done=True):
     if show_done:
         return tasks
     return [t for t in tasks if not t["done"]]
+
